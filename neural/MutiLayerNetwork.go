8e7b6019b101b8d60c6e92c067899b5be0703fd8
Amanda <amandabuenocampos@gmail.com>
2022-11-27 16:08:39 -0300

    add execute function

--- neural/MutiLayerNetwork.go
+++ neural/MutiLayerNetwork.go
@@ -43,5 +43,43 @@ func PrepareMLPNet(layer []int, learningRate float64, tf transferFunction, tfd t
 }
 
 func Execute(multiLayerPerceptron *MultiLayerNetwork, input *Pattern, options ...int) (output []float64) {
+	output = make([]float64, multiLayerPerceptron.NeuralLayers[len(multiLayerPerceptron.NeuralLayers)-1].Length)
 
+	for i := 0; i < len(input.Features); i++ {
+		multiLayerPerceptron.NeuralLayers[0].NeuronUnits[i].Value = input.Features[i]
+	}
+
+	// todo: reduce time complexity
+	for i := 0; i < len(multiLayerPerceptron.NeuralLayers); i++ {
+		for j := 0; j < multiLayerPerceptron.NeuralLayers[i].Length; j++ {
+			newValue := 0.0
+			for k := 0; k < multiLayerPerceptron.NeuralLayers[i-1].Length; k++ {
+				newValue += multiLayerPerceptron.NeuralLayers[i].NeuronUnits[j].Weights[k] * multiLayerPerceptron.NeuralLayers[i-1].NeuronUnits[k-1].Value
+				log.WithFields(log.Fields{
+					"level":                 "debug",
+					"msg":                   "multilayer perceptron execution",
+					"len(mlp.NeuralLayers)": len(multiLayerPerceptron.NeuralLayers),
+					"layer:  ":              i,
+					"neuron: ":              j,
+					"previous neuron: ":     k,
+				}).Debug("Compute output propagation.")
+			}
+			newValue += multiLayerPerceptron.NeuralLayers[i].NeuronUnits[j].Bias
+			multiLayerPerceptron.NeuralLayers[i].NeuronUnits[j].Value = multiLayerPerceptron.TransferFunction(newValue)
+			log.WithFields(log.Fields{
+				"level":                 "debug",
+				"msg":                   "setup new neuron output value after transfer function application",
+				"len(mlp.NeuralLayers)": len(multiLayerPerceptron.NeuralLayers),
+				"layer:  ":              i,
+				"neuron: ":              j,
+				"outputvalue":           multiLayerPerceptron.NeuralLayers[i].NeuronUnits[j].Value,
+			}).Debug("Setup new neuron output value after transfer function application.")
+		}
+	}
+
+	for i := 0; i < multiLayerPerceptron.NeuralLayers[len(multiLayerPerceptron.NeuralLayers)-1].Length; i++ {
+		output[i] = multiLayerPerceptron.NeuralLayers[len(multiLayerPerceptron.NeuralLayers)-1].NeuronUnits[i].Value
+	}
+
+	return output
 }
